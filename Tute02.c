96b2b216156680820e497f624a024e286152660e
It21295942 <99486720+It21295942@users.noreply.github.com>
2022-02-11 19:22:25 +0530

    Update Tute02.c

--- Tute02.c
+++ Tute02.c
@@ -17,8 +17,23 @@ Distance -> 50
 Amount = 30 x 50 + (50-30) x 40 = 2300*/
 
 #include <stdio.h>
-
-int main() {
+int main()
+{
+    int d,tot=0;
   
-  return 0;
+
+    printf("Enter traveled distance(km) : ");
+    scanf("%d",&d);
+    if(d<=30)
+    {
+    	tot= 50*d;//50/= for first 30km
+	}
+	else
+	{
+		tot= 30*50+(d-30)*40;//40/= for additional km
+	}
+    
+    printf("Total price is :%d/=",tot);
+
+    return 0;
 }
